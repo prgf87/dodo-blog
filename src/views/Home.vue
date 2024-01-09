0ef629ee21ab89b6c98186b72bc195569d06d593
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-09 18:10:32 +0000

    cleanup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,26 +6,10 @@
     <div v-for="match in matches" :key="match">{{ match }}</div>
     <button @click="handleClick">stop watching</button>
   </div>
-  <!-- <div >
-    <p ref="p">
-      <h1>Hello {{ personOne.name }}</h1>
-      Hi, my name is {{ personOne.name }} and I am {{ personOne.age }}
-    </p>
-    <input v-model="personOne.name" type="text" />
-    <button @click="handleClick">Name</button>
-    <button @click="personOne.age++">+</button>
-    <br />
-    <h1>Reactive</h1>
-    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
-    <button @click="handleChange">Name</button>
-  </div> -->
 </template>
 
 <script setup>
 import { ref, reactive, computed, watch, watchEffect } from "vue";
-
-const personOne = ref({ name: "Pedro", age: 37 });
-const personTwo = reactive({ name: "Terry", age: 25 });
 
 const skills = ref(["css", "html", "javascript"]);
 
@@ -43,17 +27,8 @@ const matches = computed(() => {
   return skills.value.filter((skill) => skill.includes(search.value));
 });
 
-// const job = computed(() => {
-//   return { role: "developer", skills: ["css", "html", "javascript"], xp: 5 };
-// });
-
-const p = ref(null);
-
 function handleClick() {
   stopWatch();
   stopEffect();
 }
-// function handleChange() {
-//   personTwo.name = "Gilbert";
-// }
 </script>
