c4b3238a632e3848ec6e43e83f4bf06af4f02fcd
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-09 17:18:45 +0000

    using setup

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="home">
-    <h1>Hello Boys!</h1>
+    <h1>Hello {{ name }}</h1>
+    <input type="text" value="name" />
+    <button @click="handleClick">Change Name</button>
   </div>
 </template>
 
 <script setup>
-let name = "Home";
-console.log(name);
+let name = "Pedro";
+
+function handleClick() {
+  console.log("clicked!");
+}
 </script>
