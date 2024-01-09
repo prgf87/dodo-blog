043327c862d15f923cc1b90e097d43c1d35a38e6
Pedro <50713178+prgf87@users.noreply.github.com>
2024-01-09 17:55:42 +0000

    working with computed values

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,44 @@
 <template>
   <div class="home">
-    <h1>Hello {{ name }}</h1>
-    <p ref="p">Hi, my name is {{ name }} and I am {{ age }}</p>
-    <input v-model="name" type="text" />
-    <button @click="age++">+</button>
+    <input type="text" v-model="search" />
+    <p>Search term - {{ search }}</p>
+    <div v-for="skill in skills" :key="skill">{{ skill }}</div>
   </div>
+  <!-- <div >
+    <p ref="p">
+      <h1>Hello {{ personOne.name }}</h1>
+      Hi, my name is {{ personOne.name }} and I am {{ personOne.age }}
+    </p>
+    <input v-model="personOne.name" type="text" />
+    <button @click="handleClick">Name</button>
+    <button @click="personOne.age++">+</button>
+    <br />
+    <h1>Reactive</h1>
+    <p>{{ personTwo.name }} - {{ personTwo.age }}</p>
+    <button @click="handleChange">Name</button>
+  </div> -->
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { ref, reactive, computed } from "vue";
 
-const name = ref("Pedro");
-const age = ref(55);
+const personOne = ref({ name: "Pedro", age: 37 });
+const personTwo = reactive({ name: "Terry", age: 25 });
+
+const skills = ["css", "html", "javascript"];
+
+const search = ref("");
+
+const job = computed(() => {
+  return { role: "developer", skills: ["css", "html", "javascript"], xp: 5 };
+});
 
 const p = ref(null);
 
 function handleClick() {
-  name.value = "Gilbert";
+  personOne.value.name = "Gilbert";
+}
+function handleChange() {
+  personTwo.name = "Gilbert";
 }
 </script>
